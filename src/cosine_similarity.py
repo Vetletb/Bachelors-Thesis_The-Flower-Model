64e226e43c02e22a5328d054267e443c0a3c851a
Vetle <vetletb@stud.ntnu.no>
2026-02-05 07:14:06 +0100

    dataloader moved to seperate file

--- src/cosine_similarity.py
+++ src/cosine_similarity.py
@@ -4,9 +4,7 @@ def cosine_similarity(a_normalized: torch.Tensor, b:torch.Tensor, abs:torch.Tens
 
     abs_flatten = abs.view(-1, 1)
     a_normalized_flatten = a_normalized.view(-1, 1)
-    print(a_normalized_flatten.shape)
     b_new = b * abs_flatten
-    print(b.shape)
     b_new_norm = torch.linalg.vector_norm(b_new, dim=-2)
 
     return torch.linalg.vecdot(a_normalized_flatten, b_new, dim=-2) / b_new_norm
---
+++ src/data.py
@@ -0,0 +1,13 @@
+from torchvision import transforms 
+from torchvision.datasets import ImageFolder
+import torch
+
+def get_dataloader(path: str, batch_size: int, img_res: int) -> torch.utils.data.DataLoader:
+    transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((img_res, img_res)),
+    transforms.ToTensor()
+    ])
+    dataset = ImageFolder(root=path, transform=transform)
+    loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    return loader
