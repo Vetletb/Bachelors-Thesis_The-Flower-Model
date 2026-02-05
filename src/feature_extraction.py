b99ea3289fc99410e5fa883c906cedf2200c5249
Vetle <vetletb@stud.ntnu.no>
2026-02-05 06:41:32 +0100

    read dataset in batches

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,4 +1,5 @@
 from torchvision import transforms 
+from torchvision.datasets import ImageFolder
 from PIL import Image
 from external.filterbank import filterbank
 from src.cosine_similarity import cosine_similarity
@@ -12,6 +13,15 @@ def image_to_tensor(file_name: str):
         ])
 
     return transform(img)
+
+transform = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.Resize((32, 32)),
+    transforms.ToTensor()
+    ])
+dataset = ImageFolder(root="dataset/train", transform=transform)
+loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False)
+images, labels = next(iter(loader))
 
 arr1, arr2, frequency_domain_sum = filterbank()
 
