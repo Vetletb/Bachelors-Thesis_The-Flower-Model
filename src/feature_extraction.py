935588fd058a7a370ac340e307105d9aad536620
Vetle <vetletb@stud.ntnu.no>
2026-02-05 06:35:19 +0100

    fix delete duplicate file

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,7 +1,7 @@
 from torchvision import transforms 
 from PIL import Image
 from external.filterbank import filterbank
-from src.feature_extraction.cosine_similarity import cosine_similarity
+from src.cosine_similarity import cosine_similarity
 import torch
 
 def image_to_tensor(file_name: str):
@@ -15,7 +15,10 @@ def image_to_tensor(file_name: str):
 
 arr1, arr2, frequency_domain_sum = filterbank()
 
+img = image_to_tensor("dataset/train/airplane/0000.jpg")
+
 filter = torch.Tensor(arr2[0].real)
 filter_norm = torch.linalg.vector_norm(filter)
 filter_normalized = filter / filter_norm
-features = cosine_similarity(filter_normalized, unfolded_img, torch.Tensor(frequency_domain_sum))
+
+features = cosine_similarity(filter_normalized, img, torch.Tensor(frequency_domain_sum))
