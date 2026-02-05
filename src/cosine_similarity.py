a4660bdabd75c88509c42cd555cfeceacc7b0cff
Vetle <vetletb@stud.ntnu.no>
2026-02-05 07:50:11 +0100

    cosine similarity support multiple filters

--- src/cosine_similarity.py
+++ src/cosine_similarity.py
@@ -1,10 +1,12 @@
 import torch
 
 def cosine_similarity(a_normalized: torch.Tensor, b:torch.Tensor, abs:torch.Tensor) -> torch.Tensor:
-
     abs_flatten = abs.view(-1, 1)
-    a_normalized_flatten = a_normalized.view(-1, 1)
+    
     b_new = b * abs_flatten
     b_new_norm = torch.linalg.vector_norm(b_new, dim=-2)
 
-    return torch.linalg.vecdot(a_normalized_flatten, b_new, dim=-2) / b_new_norm
+    patch_size = b_new.size(dim=-1)
+    batch_size = b_new.size(dim=0)
+    
+    return torch.matmul(a_normalized, b_new) / b_new_norm.view(batch_size, 1, patch_size)
