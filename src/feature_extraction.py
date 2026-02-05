20fdd2a974c6559e8d8f5cbff818e94e890ba874
Vetle <vetletb@stud.ntnu.no>
2026-02-05 06:31:00 +0100

    normalize filter

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,6 +1,7 @@
 from torchvision import transforms 
 from PIL import Image
 from external.filterbank import filterbank
+from src.feature_extraction.cosine_similarity import cosine_similarity
 import torch
 
 def image_to_tensor(file_name: str):
@@ -14,7 +15,7 @@ def image_to_tensor(file_name: str):
 
 arr1, arr2, frequency_domain_sum = filterbank()
 
-
 filter = torch.Tensor(arr2[0].real)
 filter_norm = torch.linalg.vector_norm(filter)
 filter_normalized = filter / filter_norm
+features = cosine_similarity(filter_normalized, unfolded_img, torch.Tensor(frequency_domain_sum))
