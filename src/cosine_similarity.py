127a58ad37c99f1f4bf6a0fc20e6a451f0015067
Vetle <vetletb@stud.ntnu.no>
2026-02-05 06:45:06 +0100

    reworked cosine similarity for unfolded images

--- src/cosine_similarity.py
+++ src/cosine_similarity.py
@@ -1,8 +1,12 @@
 import torch
 
 def cosine_similarity(a_normalized: torch.Tensor, b:torch.Tensor, abs:torch.Tensor) -> torch.Tensor:
-    
-    b_new = torch.mul(b, abs)
-    b_new_norm = torch.linalg.vector_norm(b_new)
 
-    return torch.linalg.vecdot(a_normalized, b_new) / b_new_norm
+    abs_flatten = abs.view(-1, 1)
+    a_normalized_flatten = a_normalized.view(-1, 1)
+    print(a_normalized_flatten.shape)
+    b_new = b * abs_flatten
+    print(b.shape)
+    b_new_norm = torch.linalg.vector_norm(b_new, dim=-2)
+
+    return torch.linalg.vecdot(a_normalized_flatten, b_new, dim=-2) / b_new_norm
--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -37,4 +37,4 @@ filter = torch.Tensor(arr2[0].real)
 filter_norm = torch.linalg.vector_norm(filter)
 filter_normalized = filter / filter_norm
 
-features = cosine_similarity(filter_normalized, img, torch.Tensor(frequency_domain_sum))
+features = cosine_similarity(filter_normalized, unfolded_img, torch.Tensor(frequency_domain_sum))
