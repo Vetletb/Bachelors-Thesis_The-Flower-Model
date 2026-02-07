40bb1c89e081c7b7cf1724754b5b8e915e7e8d3f
Vetle <vetletb@stud.ntnu.no>
2026-02-07 12:25:59 +0100

    optimize memory usage for cosine similarity

--- src/cosine_similarity.py
+++ src/cosine_similarity.py
@@ -6,12 +6,12 @@ def cosine_similarity(
 ) -> torch.Tensor:
     abs_flatten = abs.view(-1, 1)
 
-    b_new = b * abs_flatten
-    b_new_norm = torch.linalg.vector_norm(b_new, dim=-2)
+    b *= abs_flatten
+    b_norm = torch.linalg.vector_norm(b, dim=-2)
 
-    patch_size = b_new.size(dim=-1)
-    batch_size = b_new.size(dim=0)
+    patch_size = b.size(dim=-1)
+    batch_size = b.size(dim=0)
 
-    return torch.matmul(a_normalized, b_new) / b_new_norm.view(
+    return torch.matmul(a_normalized, b) / b_norm.view(
         batch_size, 1, patch_size
     )
