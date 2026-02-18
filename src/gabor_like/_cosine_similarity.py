901f780af78fa7dfb3c3f5c576b6e782c2b2e359
Vetle <vetletb@stud.ntnu.no>
2026-02-18 09:39:22 +0100

    made normalize even more memory efficient

--- src/gabor_like/_cosine_similarity.py
+++ src/gabor_like/_cosine_similarity.py
@@ -14,5 +14,6 @@ def _cosine_similarity(
 
     patch_size = b.size(dim=-1)
     batch_size = b.size(dim=0)
+    b /= b_norm.view(batch_size, 1, patch_size)
 
-    return torch.matmul(a_normalized, b) / b_norm.view(batch_size, 1, patch_size)
+    return a_normalized @ b
