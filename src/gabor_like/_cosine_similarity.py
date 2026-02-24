5c3cae69681a0efad63cadbea789aeeacc620833
florianleucht <fsleucht@stud.ntnu.no>
2026-02-24 13:52:02 +0100

    compute cos_sim with abs for each filter

--- src/gabor_like/_cosine_similarity.py
+++ src/gabor_like/_cosine_similarity.py
@@ -4,16 +4,32 @@ import torch
 def _cosine_similarity(
     a_normalized: torch.Tensor, b: torch.Tensor, abs: torch.Tensor
 ) -> torch.Tensor:
-    abs_flatten = abs.view(-1, 1)
+    
+    cos_sim_list = []
 
-    b *= abs_flatten
+    filter_amount = abs.size(dim=0)
+    for i in range(filter_amount):
+        real = a_normalized[i]
+        imag = a_normalized[i + filter_amount]
+        current_filters = torch.stack([real, imag, -real, -imag])
 
-    dtype = b.dtype
-    eps = torch.finfo(dtype).eps
-    b_norm = torch.linalg.vector_norm(b, dim=-2).clamp_(min=eps)
+        current_abs = abs[i].view(-1, 1)
 
-    patch_size = b.size(dim=-1)
-    batch_size = b.size(dim=0)
-    b /= b_norm.view(batch_size, 1, patch_size)
+        b *= current_abs
 
-    return a_normalized @ b
+        dtype = b.dtype
+        eps = torch.finfo(dtype).eps
+        b_norm = torch.linalg.vector_norm(b, dim=-2).clamp_(min=eps)
+
+        patch_size = b.size(dim=-1)
+        batch_size = b.size(dim=0)
+        b /= b_norm.view(batch_size, 1, patch_size)
+
+        cos_sim = current_filters @ b
+        cos_sim_list.append(cos_sim)
+        
+
+    cos_sim = torch.hstack(cos_sim_list)
+    print(cos_sim.shape)
+
+    return 0
