264d50816749d4cf181ad733262f31f6187d60f4
fsleucht <fsleucht@stud.ntnu.no>
2026-03-03 13:30:26 +0100

    shifting filters in x and y axis

--- src/gabor_like/_filterbank.py
+++ src/gabor_like/_filterbank.py
@@ -6,11 +6,15 @@ from torch.nn.functional import normalize
 SPH_KMEANS_ITERS = 1000
 
 
-def _create_filterbank(N: int, sigma: float, k: int, device: str) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+def _create_filterbank(
+    N: int, sigma: float, k: int, device: str
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     filters = _gabor_like_filters(N, sigma)
 
     filters = filters.astype("complex64")
     filters = torch.tensor(filters, device=device)
+
+    filters = _shift_filters(N, filters)
 
     abs_filters = torch.abs(filters)
 
@@ -30,3 +34,50 @@ def _create_filterbank(N: int, sigma: float, k: int, device: str) -> tuple[torch
     filters_normalized = normalize(filters, dim=1)
 
     return filters_normalized, abs_filters, labels
+
+
+def _shift_filters(N: int, filters: torch.Tensor):
+    steps = 5
+    step_size = int((N - 1) / (2 * steps) * 0.5)
+
+    filters_amount = filters.size(dim=0)
+
+    filters_list = [filters]
+
+    for step in range(1, steps):
+        current_step = step_size * step
+
+        left_shift = torch.cat(
+            [
+                filters[:, :, current_step:],
+                torch.zeros((filters_amount, N, current_step), dtype=filters.dtype),
+            ],
+            dim=2,
+        )
+        filters_list.append(left_shift)
+        up_shift = torch.cat(
+            [
+                filters[:, current_step:],
+                torch.zeros((filters_amount, current_step, N), dtype=filters.dtype),
+            ],
+            dim=1,
+        )
+        filters_list.append(up_shift)
+        right_shift = torch.cat(
+            [
+                torch.zeros((filters_amount, N, current_step), dtype=filters.dtype),
+                filters[:, :, :-current_step],
+            ],
+            dim=2,
+        )
+        filters_list.append(right_shift)
+        down_shift = torch.cat(
+            [
+                torch.zeros((filters_amount, current_step, N), dtype=filters.dtype),
+                filters[:, :-current_step],
+            ],
+            dim=1,
+        )
+        filters_list.append(down_shift)
+
+    return torch.vstack(filters_list)
