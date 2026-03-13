4f94784d98147075fe387672283862224252d338
Vetle <vetletb@stud.ntnu.no>
2026-03-13 13:17:16 +0100

    added CoeffProcessor to __init__

--- src/gabor_like/__init__.py
+++ src/gabor_like/__init__.py
@@ -1,5 +1,6 @@
 # gabor_like/__init__.py
 
 from .feature_extraction import FeatureExtractor
+from .process_coeff import CoeffProcessor
 
-__all__ = ["FeatureExtractor"]
+__all__ = ["FeatureExtractor", "CoeffProcessor"]
