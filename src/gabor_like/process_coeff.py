437a56ade6bc9917da7504194bfe710518871754
Vetle <vetletb@stud.ntnu.no>
2026-03-13 13:16:03 +0100

    preprocess cluster labels

--- src/gabor_like/process_coeff.py
+++ src/gabor_like/process_coeff.py
@@ -3,16 +3,11 @@ import torch
 
 from gabor_like.external._gabor_like_filters import _gabor_like_filters
 from gabor_like._kmeans import _spherical_kmeans
+from gabor_like._filterbank import _shift_filters
 
 
 class CoeffProcessor:
-    def __init__(
-        self,
-        path: str,
-        img_res: int,
-        sigma: float,
-        k: int
-    ):
+    def __init__(self, path: str, img_res: int, sigma: float, k: int):
         self.coeff_path = os.path.join(path, "extracted_features")
         self.num_batches = len(os.listdir(self.coeff_path))
         self.img_res = img_res
@@ -22,18 +17,43 @@ class CoeffProcessor:
         # Select device to run tensors on: CPU or CUDA
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def process(self):
+    def process(self, steps):
         if self.img_res % 2 == 0:
             N = self.img_res - 1
         else:
             N = self.img_res
-        
+
         filters = _gabor_like_filters(N, self.sigma)
         filters = filters.astype("complex64")
         filters = torch.tensor(filters, device=self.device)
+
+        filters = _shift_filters(filters, steps, self.img_res)
         abs_filters = torch.abs(filters)
-        abs_filters = abs_filters.view(abs_filters.shape(0), -1)
-        
+        abs_filters = abs_filters.view(abs_filters.size(dim=0), -1)
+
+        shift_width = 2 * steps if self.img_res % 2 == 0 else (2 * steps + 1)
+        shift_amount = shift_width**2
+        total_filters_amount = filters.size(dim=0)
+
+        cluster_labels = _spherical_kmeans(self.k, abs_filters, 1000)
+
+        cluster_labels_list = []
+        for i in range(shift_amount):
+            for j in range(i, total_filters_amount, shift_amount):
+                cluster_labels_list.append(cluster_labels[j])
+
+        cluster_labels = torch.hstack(cluster_labels_list)
+
+        actual_k = torch.unique(cluster_labels).numel()
+
+        cluster_labels = cluster_labels.view(-1, shift_width, shift_width)
+        pad_length = (self.img_res - shift_width) // 2
+        padder = torch.nn.ConstantPad2d(pad_length, self.k)
+        cluster_labels = padder(cluster_labels)
+        cluster_labels = cluster_labels.view(-1, self.img_res * self.img_res)
+
+        cluster_labels_exp = cluster_labels.repeat_interleave(2)  # (labels)
+
         cluster_labels = _spherical_kmeans(self.k, abs_filters, 1000)
 
         for i in range(self.num_batches):
